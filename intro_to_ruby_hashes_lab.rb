c64692d132550410705963bffca13e7e7c000658
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-11 19:04:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   base = {
-    :railroads => {:pieces => 4, :rent_in_dollars => {},:names => {}
+    :railroads => {:pieces => 4, :rent_in_dollars => {:one_piece_owned => 25},:names => {}
     }
   }
 end
